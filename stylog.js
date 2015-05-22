e5289784d532ba070a3b4241ef8dae5a594406df
FabioVergani <fabio.vergani.1975@gmail.com>
2015-05-22 21:26:20 +0200

    Update stylog.js

--- stylog.js
+++ stylog.js
@@ -2,6 +2,21 @@
 var callable=(function(o){var b=o.bind;return b.call(b,o.call);})(Function.prototype);
 var Slice=callable(Array.prototype.slice);
 
+
+function sty(x){return function(){console.log('%c'+Slice(arguments).join('\t'),x);};}
+
+log={
+	comment: sty('font-size:9px;color:#D6D6D6;')
+};
+
+log.comment(46757567567,88,88,77);
+
+
+
+==============
+var callable=(function(o){var b=o.bind;return b.call(b,o.call);})(Function.prototype);
+var Slice=callable(Array.prototype.slice);
+
 function stylog(css){
  var o=console,s=css;
  return function(){
